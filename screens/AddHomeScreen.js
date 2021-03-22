228cc0043ccfdb82be9112164d0221f666994086
Tim Keresey <tim.keresey@gmail.com>
2021-03-22 16:56:38 -0600

    add styles to Add Home Screen

--- screens/AddHomeScreen.js
+++ screens/AddHomeScreen.js
@@ -1,18 +1,85 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, TextInput, Button} from 'react-native';
 
 const AddHomeScreen = () => {
 
     return (
-        <View>
-            <Text>
-                Add Home Screen
-            </Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <View style={styles.formGroup}>
+                    <Text style={styles.label}>Title</Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+                <View style={styles.formGroup}>
+                    <Text style={styles.label}>Home Type</Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+                <View style={styles.formGroup}>
+                    <Text style={styles.label}>Price</Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+                <View style={styles.formGroup}>
+                    <Text style={styles.label}>Year Built</Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+                <View style={styles.formGroup}>
+                    <Text style={styles.label}>Address</Text>
+                    <TextInput
+                        style={styles.input}
+                        multiline
+                    />
+                </View>
+                <View style={styles.formGroup}>
+                    <Text style={styles.label}>Description</Text>
+                    <TextInput
+                        style={styles.input}
+                        multiline
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        title="Add Home"
+                    />
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
 const styles =StyleSheet.create({
+    form: {
+        margin: 20,
+        backgroundColor: "#ffffff",
+        padding: 20,
+        borderRadius: 10,
+    },
+
+    formGroup: {
+        width: '100%',
+    },
+
+    label: {
+        marginVertical: 10,
+    },
+
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 8,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+    },
+
+    buttonContainer: {
+        marginTop: 20,
+    }
 
 });
 
