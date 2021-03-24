0e852b379e7941d6fc214eecc03a27e3af0568be
Tim Keresey <tim.keresey@gmail.com>
2021-03-24 16:41:12 -0600

    fetch data from api

--- redux/actions/houseAction.js
+++ redux/actions/houseAction.js
@@ -4,11 +4,14 @@ export const CREATE_HOUSES = 'CREATE_HOUSES';
 export const fetchHouses = () => {
     return async dispatch => {
 
-        
+        // logic that connects FE to API
+        const result = await fetch('http://localhost:3000/api/houses');
+
+        const resultData = await result.json();
 
         dispatch({
             type: FETCH_HOUSES,
-            payload: 1
-        })
+            payload: resultData
+        });
     }
 }
--- screens/HomeListScreen.js
+++ screens/HomeListScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {FloatingAction} from 'react-native-floating-action';
+import {useDispatch} from 'react-redux';
 
 import Card from '../components/Card';
+import * as houseAction from '../redux/actions/houseAction';
 
 const HomeListScreen = (props) => {
+
+    const dispatch = useDispatch();
+
+    const test = () => {
+        fetch('http://localhost:3000/api/houses')
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        test();
+    })
+    // useEffect(() => {
+    //     dispatch(houseAction.fetchHouses());
+    // }, [dispatch]);
 
     return (
         <View style={styles.container}>
